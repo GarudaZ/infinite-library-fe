aaff670b1e0ce398e6dc8ca5a4e9592eb467f8b8
Gareth A <garethjaustin@gmail.com>
2024-08-22 16:02:02 +0100

    initial, including routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SplashComponent } from './splash/splash.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+export const routes: Routes = [
+  { path: 'login-component', component: LoginComponent },
+  { path: 'splash-component', component: SplashComponent },
+  { path: 'home-component', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
