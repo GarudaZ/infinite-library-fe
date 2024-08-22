d004636347339e38dd4bf4c411b2150431c8b2fc
Gareth A <garethjaustin@gmail.com>
2024-08-22 16:12:15 +0100

    adds a 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { SplashComponent } from './splash/splash.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 export const routes: Routes = [
-  { path: 'login-component', component: LoginComponent },
-  { path: 'splash-component', component: SplashComponent },
-  { path: 'home-component', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: SplashComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
